260a54b90198b37f409ecfd2645dabef90438204
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-14 19:28:37 +0700

    update hw03-2

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -1,26 +1,26 @@
 //TODO: #9 Fix bug and arrange code in CODEX pattern.
 #include <stdio.h>  
-#include <stdbool.h> 
+#include <stdbool.h> //The Standard Boolean Library section files are used in the logic section.
 
 int main() {
-    float a, b = 3 ;
+    float a, b = 3 ; //The variable b is float with a default value of 3.0.
     bool _1 = false ; 
    
     printf( "%d ", _1) ;
     int i = 0 ;
     
     if ( _1 != false) { 
-        printf( "ok") ;
-    } else {
-        while ( i <= 5) {
-            printf( "not-okay-%d " , ++i) ;
+        printf( "ok") ;//If the condition on line 12 is true, the message "ok" is displayed.
+    } else { //It is the one that runs when the condition on line 12 is false.
+        while ( i <= 5) { //is a while loop that runs when i is less than or equal to 5.
+            printf( "not-okay-%d " , ++i) ; //If the condition on line 15 is true, the message "not okay" is displayed. followed by the value of i and incrementing i by 1 as well.
         }
         
-        for ( i = 3 ; i < 10; i += 1) {
+        for ( i = 3 ; i < 10; i += 1) { //is a for loop that starts at i with a value of 3 and executes when i is less than 10, incrementing i by 1 each loop. 
             if ( i % 2 == (!_1 ? 1 : 2)) { 
-                printf( "see see") ;
+                printf( "see see") ; //If the condition in line 20 is true, the message "see see" is displayed.
             } else {
-                printf( "haha") ;
+                printf( "haha") ; //If the condition in line 20 is not true, the message "haha" is displayed.
             }//end else
         }//end for
     }//end else
